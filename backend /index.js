bb497bc774c6d9e04d0e142fb84791dfca14075d
aaadityasngh <ddeaditya@gmail.com>
2026-02-26 10:43:07 +0530

    add nodemon dependencies in dev mode

--- backend /index.js
+++ backend /index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    console.console.log("this is server");
+    console.console.log("this is server....");
 });
 
 app.listen(PORT, () => {
